0818b9dd48a2468134f878518089f11b80c428bf
shabane <m.mohamadshabane@gmail.com>
2023-08-13 10:54:31 +0330

    [feat](use telegram and save to find and save config links)

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import config
+from telegram import Telegram
+
+
+if __name__ == '__main__':
+    network01 = Telegram(channels=config.channels)
+    print(f'shadow socks: {len(network01.ss)}')
+    print(f'vmess: {len(network01.vmess)}')
+    print(f'vless: {len(network01.vless)}')
+    print(f'trojan: {len(network01.trojan)}')
+    config.save(network01)
+    
