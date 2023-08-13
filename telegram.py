b590adb8e4123eb7d2bf8b47c797365238eaae62
shabane <m.mohamadshabane@gmail.com>
2023-08-13 06:25:21 +0330

    [feat](search for protocols and assing result to its varialbe)

---
+++ telegram.py
@@ -0,0 +1,22 @@
+from config import Protocols
+from os import path
+import requests
+import re
+
+
+class Telegram(Protocols):
+    def __init__(self, channels: list) -> None:
+        self.channels = channels
+        self.__tlink = 'https://t.me/s/'
+        self.__v2finder()
+ 
+    def __v2finder(self) -> None:
+        for channel in self.channels:
+            page = requests.get(path.join(self.__tlink, channel))
+            while page.status_code != 200:
+                page = requests.get(path.join(self.__tlink, channel))
+
+            self.ss = [f'{i[0]}{i[1]}' for i in re.findall("(ss:\/\/[^\#\s\n]*)(\#[^\s\n<]+)", page.text)]
+            self.vless = [f'{i[0]}{i[1]}' for i in re.findall("(vless:\/\/[^\#\s\n]*)(\#[^\s\n<]+)", page.text)]
+            self.vmess = [f'{i[0]}{i[1]}' for i in re.findall("(vmess:\/\/[^\#\s\n]*)(\#[^\s\n<]+)", page.text)]
+            self.trojan = [f'{i[0]}{i[1]}' for i in re.findall("(trojan:\/\/[^\#\s\n]*)(\#[^\s\n<]+)", page.text)]
